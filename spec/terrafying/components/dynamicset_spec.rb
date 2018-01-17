52c413b8ab5bdc6e1f83c5e9d926a9f7bafd3f19
Piotr Komborski <piotr.komborski@gmail.com>
2018-01-17 16:30:07 +0000

    Fix adjust tests to check aws_cloudformation_stack

--- spec/terrafying/components/dynamicset_spec.rb
+++ spec/terrafying/components/dynamicset_spec.rb
@@ -11,16 +11,16 @@ RSpec.describe Terrafying::Components::DynamicSet do
     @vpc = stub_vpc("a-vpc", "10.0.0.0/16")
   end
 
-  it "should just create a single asg by default" do
+  it "should just create a single asg template by default" do
     dynamic_set = Terrafying::Components::DynamicSet.create_in(@vpc, "foo")
 
-    expect(dynamic_set.output["resource"]["aws_autoscaling_group"].count).to eq(1)
+    expect(dynamic_set.output["resource"]["aws_cloudformation_stack"].count).to eq(1)
   end
 
   it "should create an asg per availability zone when pivoting" do
     dynamic_set = Terrafying::Components::DynamicSet.create_in(@vpc, "foo", { pivot: true })
 
-    expect(dynamic_set.output["resource"]["aws_autoscaling_group"].count).to eq(@vpc.azs.count)
+    expect(dynamic_set.output["resource"]["aws_cloudformation_stack"].count).to eq(@vpc.azs.count)
   end
 
   it "should add a depend_on for the instance profile" do
